33b506f4c6ddc136fb553998bb85ae30731ca39b
Katsiaryna <katyabaranovskaya97@gmail.com>
2020-02-27 19:36:05 +0300

    refactoring the calculation of general statistics using the future

--- src/main/scala/com.itechart.citybike/GeneralStats.scala
+++ src/main/scala/com.itechart.citybike/GeneralStats.scala
@@ -8,8 +8,7 @@ import com.itechart.citybike.writer.Writer
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.logging.log4j.scala.Logging
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
@@ -24,62 +23,76 @@ object GeneralStats extends Logging {
   def main(args: Array[String]): Unit = {
     val files = reader.getListOfFiles("/" + SOURCE_DIR)
 
-    val startDate = LocalDateTime.parse("2015-08-01T00:00:00")
-    val endDate = LocalDateTime.parse("2017-08-03T00:00:00")
-
-    val futureOperations: List[Future[Int]] = files.map(file => getNumberOfTrips(file))
-    val futureSequenceResults = Future.sequence(futureOperations)
-
-    val futureOperations2: List[Future[Int]] = files.map(file => getCountLongestTrip(file))
-    val futureSequenceResults2 = Future.sequence(futureOperations2)
-
-    futureSequenceResults.onComplete {
+    val countTripsFutureSeq: List[Future[Int]] = files.map(file => countTrips(file))
+    Future.sequence(countTripsFutureSeq).onComplete {
         case Success(result) => {
-          println("result: " + result.sum)
+          println("Total number of trips: " + result.sum)
         }
       case Failure(e) => logger.error("Exception during file processing", e)
     }
 
-    futureSequenceResults2.onComplete {
+    val countLongestTripFutureSeq: List[Future[Int]] = files.map(file => countLongestTrip(file))
+    Future.sequence(countLongestTripFutureSeq).onComplete {
       case Success(result) => {
-        println("result2: " + result.max)
+        println("The time of the longest trip: " + result.max)
       }
       case Failure(e) => logger.error("Exception during file processing", e)
     }
 
-//    val res = Await.result(futureSequenceResults, Duration.Inf)
+    val startDate = LocalDateTime.parse("2015-08-01T00:00:00")
+    val endDate = LocalDateTime.parse("2017-08-03T00:00:00")
+    val countBicyclesFutureSeq: List[Future[Set[Int]]] = files.map(file => countBicycles(file, startDate, endDate))
+    Future.sequence(countBicyclesFutureSeq).onComplete {
+      case Success(result) => {
+        println("The number of bicycles used during the specified period: " + result.flatten.toSet.size)
+      }
+      case Failure(e) => logger.error("Exception during file processing", e)
+    }
+
+    val countManFutureSeq: List[Future[Int]] = files.map(file => countMan(file))
+    Future.sequence(countManFutureSeq).onComplete {
+      case Success(result) => {
+        println("The number of men using the service: " + result.sum)
+      }
+      case Failure(e) => logger.error("Exception during file processing", e)
+    }
+
+    val countWomanFutureSeq: List[Future[Int]] = files.map(file => countWoman(file))
+    Future.sequence(countWomanFutureSeq).onComplete {
+      case Success(result) => {
+        println("The number of women using the service: " + result.sum)
+      }
+      case Failure(e) => logger.error("Exception during file processing", e)
+    }
 
     Thread.sleep(300000)
-//    println(s"Future val is: ${res.flatten.toSet.size}")
   }
 
-  def getNumberOfTrips(fileName: String): Future[Int] = Future {
+  def countTrips(fileName: String): Future[Int] = Future {
     val data = reader.readFile(SOURCE_DIR + "/" + fileName).drop(1)
     data.size
   }
 
-  def getCountLongestTrip(fileName: String): Future[Int] = Future {
+  def countLongestTrip(fileName: String): Future[Int] = Future {
     val data = reader.readFile(SOURCE_DIR + "/" + fileName).drop(1)
     data.map(parseLine).maxBy(_.tripDuration).tripDuration
   }
 
-  def getCountBicycles(fileName: String, startDate: LocalDateTime, endDate: LocalDateTime): Future[Set[Int]] = Future {
+  def countBicycles(fileName: String, startDate: LocalDateTime, endDate: LocalDateTime): Future[Set[Int]] = Future {
     val data = reader.readFile(SOURCE_DIR + "/" + fileName).drop(1)
     data.map(parseLine).filter(_.startTime.isAfter(startDate)).filter(_.startTime.isBefore(endDate)).map(x => x.bikeId).toSet
   }
 
-  def getCountManPercentage(fileName: String): Future[Double] = Future {
+  def countMan(fileName: String): Future[Int] = Future {
     val data = reader.readFile(SOURCE_DIR + "/" + fileName).drop(1)
-    data.map(parseLine).count(x => x.gender == 1).toDouble
+    data.map(parseLine).count(x => x.gender == 1)
   }
 
-  def getCountWomanPercentage(fileName: String): Future[Double] = Future {
+  def countWoman(fileName: String): Future[Int] = Future {
     val data = reader.readFile(SOURCE_DIR + "/" + fileName).drop(1)
-    data.map(parseLine).count(x => x.gender == 2).toDouble
+    data.map(parseLine).count(x => x.gender == 2)
   }
 }
-
-//(data.count(x => x.gender == 1).toDouble * 100) / numberOfTrips
 
 //val generalStats = Seq(
 //      "Total number of trips - " + numberOfTrips,
